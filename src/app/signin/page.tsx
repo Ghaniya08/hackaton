472fe4c4b57775407d424da46457e79ee1d5c56f
Ghaniya khan <165418041+Ghaniya08@users.noreply.github.com>
2024-12-09 01:20:48 +0500

    Update page.tsx

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -21,7 +21,7 @@ export default function SignUpPage() {
             <li><Link href="/blog">Blog</Link></li>
             <li><Link href="/ourchef">Chef</Link></li>
             <li><Link href="/aboutus">About</Link></li>
-            <li><Link href="/ourshop">Shop</Link></li>
+            <li><Link href="/shop">Shop</Link></li>
             <li><Link href="/signin">Signin</Link></li>
             </ul>
           </nav>
