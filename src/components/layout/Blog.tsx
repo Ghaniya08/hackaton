30a08752134a05bbc99f65c1d549ffa4afbda973
Ghaniya khan <165418041+Ghaniya08@users.noreply.github.com>
2024-12-10 23:52:59 +0500

    Update Blog.tsx

--- src/components/layout/Blog.tsx
+++ src/components/layout/Blog.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link';
 import { MdThumbUpOffAlt } from "react-icons/md";
 import { LiaCommentDotsSolid } from "react-icons/lia";
 import { LuShare2 } from "react-icons/lu";
