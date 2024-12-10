65159312750c22b13580c111dc7c2ae08b5aabc3
Ghaniya khan <165418041+Ghaniya08@users.noreply.github.com>
2024-12-10 12:18:34 +0500

    Update page.tsx

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -90,7 +90,7 @@ export default function About() {
         <li><Link href="/blog">Blog</Link></li>
         <li><Link href="/ourchef">Chef</Link></li>
         <li><Link href="/aboutus">About</Link></li>
-        <li><Link href="/ourshop">Shop</Link></li>
+        <li><Link href="/shop">Shop</Link></li>
         <li><Link href="/signin">Signin</Link></li>
         </ul>
       </nav>
@@ -111,7 +111,7 @@ export default function About() {
         <li><Link href="/blog">Blog</Link></li>
         <li><Link href="/ourchef">Chef</Link></li>
         <li><Link href="/aboutus">About</Link></li>
-        <li><Link href="/ourshop">Shop</Link></li>
+        <li><Link href="/shop">Shop</Link></li>
         <li><Link href="/signin">Signin</Link></li>
         </ul>
       </SheetContent>
